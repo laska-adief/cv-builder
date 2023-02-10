04276777257dc7c4aff9fa4d2122af786bde3ae6
laska-adief <laskaadief.la@gmail.com>
2023-02-10 08:34:59 +0700

    [CV Builder] Generate component cv-builder

---
+++ src/app/cv-builder/cv-builder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cv-builder',
+  templateUrl: './cv-builder.component.html',
+  styleUrls: ['./cv-builder.component.scss']
+})
+export class CvBuilderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
