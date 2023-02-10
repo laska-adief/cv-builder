6d590944bd91eabfd156588a12c8d3f06b3b8779
laska-adief <laskaadief.la@gmail.com>
2023-02-10 08:35:16 +0700

    [CV Builder] Generate component cv-preview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CvBuilderComponent } from './cv-builder/cv-builder.component';
+import { CvPreviewComponent } from './cv-preview/cv-preview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CvBuilderComponent,
+    CvPreviewComponent
   ],
   imports: [
     BrowserModule,
