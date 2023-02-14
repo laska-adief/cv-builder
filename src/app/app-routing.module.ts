360036d1a3f2e35372d7056aa0c43457836398fb
laska-adief <laskaadief.la@gmail.com>
2023-02-14 12:17:29 +0700

    [CV Builder] routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CvBuilderComponent } from './cv-builder/cv-builder.component';
+import { CvDownloadComponent } from './cv-download/cv-download.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CvBuilderComponent},
+  {path: 'cv-download', component: CvDownloadComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
