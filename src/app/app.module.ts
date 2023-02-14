ec0fba4905cd8e1f5126b8c8e39b1e910f56a2b4
laska-adief <laskaadief.la@gmail.com>
2023-02-14 12:15:05 +0700

    [CV Builder] import required modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CvBuilderComponent } from './cv-builder/cv-builder.component';
 import { CvPreviewComponent } from './cv-preview/cv-preview.component';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { CvDownloadComponent } from './cv-download/cv-download.component';
+import { RouterModule } from '@angular/router';
+
+const angularMaterial = [
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatIconModule,
+  MatCardModule
+];
 @NgModule({
-  declarations: [
-    AppComponent,
-    CvBuilderComponent,
-    CvPreviewComponent
-  ],
+  declarations: [AppComponent, CvBuilderComponent, CvPreviewComponent, CvDownloadComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ...angularMaterial,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    RouterModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
