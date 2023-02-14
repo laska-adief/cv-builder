7cc424c5d61e2c1aaa761d97f95e040fca2d0ea5
laska-adief <laskaadief.la@gmail.com>
2023-02-14 12:15:58 +0700

    [CV Builder] UI & Functionality cv form

--- src/app/cv-builder/cv-builder.component.ts
+++ src/app/cv-builder/cv-builder.component.ts
@@ -1,15 +1,82 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CvServiceService } from '../services/cv-service.service';
 
 @Component({
   selector: 'app-cv-builder',
   templateUrl: './cv-builder.component.html',
-  styleUrls: ['./cv-builder.component.scss']
+  styleUrls: ['./cv-builder.component.scss'],
 })
 export class CvBuilderComponent implements OnInit {
+  cvForm!: FormGroup;
+  cvFormValue: any;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private cvService: CvServiceService) {}
 
   ngOnInit(): void {
+    this.formInit();
+
+    // set form based on value saved in service
+    const servieValue = this.cvService.getCv();
+    this.cvForm.patchValue(servieValue);
+
+    // update value input to cv preview
+    this.cvForm.valueChanges.subscribe((val) => {
+      this.cvFormValue = val;
+    });
   }
 
+  formInit() {
+    this.cvForm = this.fb.group({
+      name: [''],
+      telp: [''],
+      email: ['', Validators.email],
+      address: [''],
+      about: [''],
+      experience: this.fb.array([]),
+      education: this.fb.array([]),
+    });
+
+    this.addCvExperience();
+    this.addCvEducation();
+  }
+
+  getCvExperience() {
+    return this.cvForm.get('experience') as FormArray;
+  }
+
+  initCvExperience() {
+    return this.fb.group({
+      company: [''],
+      title: [''],
+      location: [''],
+      startDate: [''],
+      endDate: [''],
+      description: [''],
+    });
+  }
+
+  addCvExperience() {
+    this.getCvExperience().push(this.initCvExperience());
+  }
+
+  getCvEducation() {
+    return this.cvForm.get('education') as FormArray;
+  }
+
+  initCvEducation() {
+    return this.fb.group({
+      school: [''],
+      degree: [''],
+      field: [''],
+      grade: [''],
+      startDate: [''],
+      endDate: [''],
+      description: [''],
+    });
+  }
+
+  addCvEducation() {
+    this.getCvEducation().push(this.initCvEducation());
+  }
 }
