66a11aa9e673cd2714a12c2fdca3d2fe27f9132f
laska-adief <laskaadief.la@gmail.com>
2023-02-14 12:16:56 +0700

    [CV Builder] UI & Functionality cv preview

--- src/app/cv-preview/cv-preview.component.ts
+++ src/app/cv-preview/cv-preview.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
+import { CvServiceService } from '../services/cv-service.service';
 
 @Component({
   selector: 'app-cv-preview',
   templateUrl: './cv-preview.component.html',
-  styleUrls: ['./cv-preview.component.scss']
+  styleUrls: ['./cv-preview.component.scss'],
 })
-export class CvPreviewComponent implements OnInit {
-
-  constructor() { }
+export class CvPreviewComponent implements OnInit, OnChanges {
+  @Input() formValue: any;
+  constructor(private router: Router, private cvService: CvServiceService) {}
 
   ngOnInit(): void {
+    const value = this.cvService.getCv();
+    if(value) {
+      this.formValue = value;
+    }
+    console.log('input', this.formValue) ;
   }
 
+  ngOnChanges(change: SimpleChanges) {
+    console.log("CHANGES", change)
+    console.log('Changes', this.formValue);
+  }
+
+  downloadCv() {
+    this.cvService.setCv(this.formValue);
+    this.router.navigate(['/cv-download']);
+  }
 }
